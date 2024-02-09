85553547862e623725aa8b763fefae9040f20268
Andres Puchigay <apuchigay@gmail.com>
2024-02-09 14:54:33 -0500

    Deteccion de colision con los bordes superior e inferior del campo

--- Day 22 - Pong Game/ball.py
+++ Day 22 - Pong Game/ball.py
@@ -8,8 +8,23 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.shapesize(stretch_wid=1, stretch_len=1)
+        # Define las direcciones de X e Y
+        self.direction_y = 10
+        self.direction_x = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.direction_x
+        new_y = self.ycor() + self.direction_y
         self.goto(new_x, new_y)
+        self.bounce()
+
+    def bounce(self):
+        """Válida si la posición del objeto es superior al límite superior e inferior para despues invertir sus
+        posiciones"""
+        if self.ycor() > 280:
+            self.sety(280)
+            self.direction_y *= -1
+
+        if self.ycor() < -280:
+            self.sety(-280)
+            self.direction_y *= -1
